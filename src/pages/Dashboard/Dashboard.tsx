df23f0bcc2156ec0ce5afa40a9cd0b9995ed732b
ramieeee <rambotty@gmail.com>
2023-05-14 21:41:02 +0900

    Add GET locations api with useState

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
+import { Location } from "mocks/db";
 import "./Dashboard.css";
 
 export default function Dashboard(): JSX.Element {
+  const [locations, setLocations] = useState<Location[]>();
+
   const getLocation = async () =>
     await fetch("/locations")
       .then(async (data) => {
-        const temp = await data.json();
-        console.log(temp);
+        const locationData = await data.json();
+        setLocations(locationData);
       })
       .catch((e) => {
         alert("Error getting locations. Please try again");
       });
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   return (
     <div className="Dashboard">
-      <button onClick={getLocation}>test</button>
+      <button onClick={() => console.log(locations)}>test</button>
       <div className="header">Your Fleet</div>
       <div className="search_filter_container"></div>
       <div className="table_container"></div>
